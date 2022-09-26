079189da66e7466a25cedd48d7a94d594947901c
Ram-Charan-boddupally <66841463+Ram-Charan-boddupally@users.noreply.github.com>
2022-09-26 23:54:48 +0530

    Added eventListners, validation functions
    1. Added event listener to the tags type=radio and select tag
    2. Made changes in the header modified font to 'century gothic'
    3. Added href attribute value to index page in task1, career and contact us page
    4. Added custom styling to text area,radio buttons
    5. Added reset function for the page

--- task3/scripts/script.js
+++ task3/scripts/script.js
@@ -1,31 +1,84 @@
-function checkEmptyElements(form){
-    let count = 0 ,spanElement;
-    if(form.querySelector("#name").value == ""){
-        spanElement = form.querySelector(".name-field .user-message");
-        spanElement.innerHTML = "Name is required";
-        count += 1;
+// function fot validation of the forms emailIds ,names, Empty values 
+function reset(form){
+    form.querySelectorAll('input').forEach((ele)=>{
+        ele.value = "";
+    })
+    if(form.contains('input[type=radio]')){
+        form.querySelectorAll('input[type=radio]').checked = false;
     }
-    if(form.querySelector("#emailId").value == ""){
-        spanElement = form.querySelector(".email-field .user-message");
-        spanElement.innerHTML = "EmailId is required";
-        count += 1;
+    if(form.contains('textarea')){
+        form.querySelectorAll('textarea').forEach(ele => {ele.innerHTML = ""})
     }
-    if(form.querySelector("#desiredRole").value == ""){
-        spanElement = form.querySelector(".role-field .user-message");
-        spanElement.innerHTML = "Desired Role is required";
-        count += 1;
-
+    if(form.contains('select')){
+        form.querySelector('select').selectedIndex = 0;
     }
-    if(form.querySelector("#resume").files.length == 0){
-        console.log("helo");
-        spanElement = form.querySelector(".resume-field .user-message");
-        spanElement.innerHTML = "Please Upload your resume";
-        count += 1;
-    }
-    if(count==0){
-        document.querySelector("#succesfullSubmittion").style.visibility = "visible";
-        document.querySelector("#succesfullSubmittion span").innerHTML = form.querySelector("#name").value; 
-    }
-    form.preventDefault();
-    // return false;
 }
+
+
+function validateForm(form){    
+    form.querySelectorAll(".imp-field").forEach((element)=>{
+        let value = element.querySelector('input').value;
+        if(value == ""){
+            element.querySelector('.user-msg').innerHTML = "Field Cant Be Empty";
+        }else{
+            if(element.classList.contains('name-field')){
+                for(let ele of value){
+                    if(ele > '0' && ele<'9'){
+                        element.querySelector('.user-msg').innerHTML = "Field Canont contain numerics";
+                        break;                   
+                    }
+                }
+            }else if(element.classList.contains('email-field')){
+                console.log(value);
+                let re = /(^[a-zA-Z]+)([_\.-]|[0-9a-zA-Z])*@([_-]|[0-9a-zA-Z])*(\.[a-zA-Z0-9]+){2}/;
+                if(!value.match(re)){
+                    element.querySelector('.user-msg').innerText = "Please enter correct format of email";
+                }
+            }
+        }
+    })
+} 
+
+(function addMandatorySign(){
+    ne_fields = document.getElementsByClassName('imp-field');
+    for(let i=0;i<ne_fields.length;i++){
+        let para = document.createElement('span');
+        para.setAttribute('class','user-msg');
+        para.innerText = "*";
+        ne_fields[i].appendChild(para);
+    }    
+})();
+
+(function addEventListners(){
+    let stateElement = document.querySelector('#stateName');
+    if(document.contains(stateElement)){
+        stateElement.addEventListener('change',(event)=>{
+            document.getElementById('promotionCode').value = event.target.value+"-PROMO";
+        });    
+    }
+
+    let inputElements = document.querySelectorAll("input[name=gender]");
+    if(document.contains(inputElements[0])){
+
+        inputElements.forEach((element)=> {
+            element.addEventListener('click',(event)=>{
+                pnoun = ""
+                if(event.target.value == "Male"){
+                    pnoun = "Sir";
+                }else{
+                    pnoun = "Madam";
+                }
+                alert("Hello "+pnoun);
+            })
+        })
+    }
+    let fileElement = document.querySelector('#resume')
+    if(document.contains(fileElement)){
+        fileElement.addEventListener('onclick',event=>{
+            console.log("ram");
+            fileElement = fileElement.value;
+            fileElement.style.contentVisibility = "visible";
+        })
+    }
+})();
+
