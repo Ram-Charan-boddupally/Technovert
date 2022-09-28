6a5f1e69c98ec5c72b2ef80fc8275ed7941f74a2
Ram-Charan-boddupally <66841463+Ram-Charan-boddupally@users.noreply.github.com>
2022-09-28 09:56:46 +0530

    Added main function and focusout listner
    Created the main function and called it using on load event listener
    the main function is used to call the event listeners and mandatory calls
    Validated the email field before submission of the form

--- task3/scripts/script.js
+++ task3/scripts/script.js
@@ -1,5 +1,6 @@
 // function fot validation of the forms emailIds ,names, Empty values 
 function reset(form){
+    console.log("ram");
     form.querySelectorAll('input').forEach((ele)=>{
         ele.value = "";
     })
@@ -12,34 +13,42 @@ function reset(form){
     if(form.contains('select')){
         form.querySelector('select').selectedIndex = 0;
     }
+    document.getElementById("unSuccesfullSubmittion").style.display = 'none';
+    document.getElementById("succesfullSubmittion").style.display = 'none';
 }
 
 
-function validateForm(form){    
+function validateForm(form){
+
+    let status = true;
     form.querySelectorAll(".imp-field").forEach((element)=>{
         let value = element.querySelector('input').value;
         if(value == ""){
             element.querySelector('.user-msg').innerHTML = "Field Cant Be Empty";
+            status = false;
         }else{
             if(element.classList.contains('name-field')){
                 for(let ele of value){
                     if(ele > '0' && ele<'9'){
                         element.querySelector('.user-msg').innerHTML = "Field Canont contain numerics";
+                        status = false;
                         break;                   
                     }
                 }
-            }else if(element.classList.contains('email-field')){
-                console.log(value);
-                let re = /(^[a-zA-Z]+)([_\.-]|[0-9a-zA-Z])*@([_-]|[0-9a-zA-Z])*(\.[a-zA-Z0-9]+){2}/;
-                if(!value.match(re)){
-                    element.querySelector('.user-msg').innerText = "Please enter correct format of email";
-                }
+                // website and number via js 
             }
         }
     })
+    if(!status){
+        document.getElementById("unSuccesfullSubmittion").style.color = 'red';
+        document.getElementById("unSuccesfullSubmittion").style.display = 'initial';
+    }else{
+        document.getElementById("succesfullSubmittion").style.color = 'green';
+        document.getElementById("succesfullSubmittion").style.display = 'initial';
+    }
 } 
 
-(function addMandatorySign(){
+function addMandatorySign(){
     ne_fields = document.getElementsByClassName('imp-field');
     for(let i=0;i<ne_fields.length;i++){
         let para = document.createElement('span');
@@ -47,9 +56,9 @@ function validateForm(form){
         para.innerText = "*";
         ne_fields[i].appendChild(para);
     }    
-})();
+}
 
-(function addEventListners(){
+function addEventListners(){
     let stateElement = document.querySelector('#stateName');
     if(document.contains(stateElement)){
         stateElement.addEventListener('change',(event)=>{
@@ -75,10 +84,27 @@ function validateForm(form){
     let fileElement = document.querySelector('#resume')
     if(document.contains(fileElement)){
         fileElement.addEventListener('onclick',event=>{
-            console.log("ram");
             fileElement = fileElement.value;
             fileElement.style.contentVisibility = "visible";
         })
     }
-})();
 
+    let emailElement = document.querySelector('.email-field');
+    if(document.contains(emailElement)){
+        const re = /(^[a-zA-Z]+)([_\.-]|[0-9a-zA-Z])*@([_-]|[0-9a-zA-Z])*(\.[a-zA-Z0-9]+){2}/;
+        emailElement.addEventListener('focusout',function(){
+            let value = emailElement.querySelector('input').value;
+            if(!value.match(re)){
+                emailElement.querySelector('.user-msg').innerText = "Please enter correct format of email";
+            }    
+        });
+    }
+};
+
+
+function main(){
+    addMandatorySign();
+    addEventListners();   
+}
+
+document.addEventListener('load',main());
